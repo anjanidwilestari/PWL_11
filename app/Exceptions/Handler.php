45c58f2ab04dfb6ac1b9a92c6021650c25057b49
Ayi <ayianjani774@gmail.com>
2022-04-26 16:53:29 +0700

    update function render

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,6 +2,8 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Http\Response;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
 
@@ -46,5 +48,16 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, $exception)
+    {
+        if($exception instanceof ModelNotFoundException && $request->wantsJson()){
+            return response()->json(
+                ['message'=>'Not Found!'],
+                Response::HTTP_NOT_FOUND
+            );
+        }
+        return parent::render($request, $exception);
     }
 }
