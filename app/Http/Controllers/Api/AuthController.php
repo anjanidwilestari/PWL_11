3eda2bae7fd8d6856621df9f412dd9fd78948991
Ayi <ayianjani774@gmail.com>
2022-04-26 15:35:37 +0700

    update function logout

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -7,10 +7,12 @@ use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use App\Traits\ApiResponse;
+use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Throwable;
 
 class AuthController extends Controller
 {
@@ -48,5 +50,21 @@ class AuthController extends Controller
             'token_type'=>'Bearer',
             'user'=>$user,
         ]);
+    }
+
+    public function logout()
+    {
+        try
+        {
+            auth()->user()->tokens()->delete();
+            return $this->apiSuccess('Tokens revoked');
+        } 
+        catch(Throwable $e)
+        {
+            throw new HttpResponseException($this->apiError(
+                null,
+                Response::HTTP_INTERNAL_SERVER_ERROR,
+            ));
+        }
     }
 }
